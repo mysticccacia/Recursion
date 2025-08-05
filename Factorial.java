543b7ac9f8792a913ab6d21406ace6a7aa36be85
Abhishek Kushwaha <152796449+mysticcasia@users.noreply.github.com>
2025-08-05 12:01:48 +0530

    changed

--- Factorial.java
+++ Factorial.java
@@ -8,7 +8,7 @@ public class Factorial {
     }
 
     static int fact(int n){                     
-        if(n==1){
+        if(n== 1){
             return 1;
         }
         return n * fact(n-1);
